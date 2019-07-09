0d01f16a63af79ef7298d9be960f4a41b10ada3c
Shamyl Zakariya <shamyl@gmail.com>
2019-07-09 09:28:59 -0700

    Add a separator rule to the exclusion section header

--- src/BatchController/BatchCollectionViewSectionHeader.m
+++ src/BatchController/BatchCollectionViewSectionHeader.m
@@ -8,4 +8,15 @@
 #import "BatchCollectionViewSectionHeader.h"
 
 @implementation BatchCollectionViewSectionHeader
+
+- (void)drawRect:(NSRect)dirtyRect {
+    if (@available(macOS 10.13, *)) {
+        [[NSColor colorNamed:@"BatchCollectionViewSectionHeaderRule"] set];
+    } else {
+        // previous macos versions don't have dark mode, so we'll just hardcode a light gray
+        [[NSColor colorWithWhite:0.9 alpha:1] set];
+    }
+    NSRectFill(NSMakeRect(0, self.bounds.size.height - 1, self.bounds.size.width, 1));
+}
+
 @end
